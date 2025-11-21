b8ec1b0fa79bbdd6b3b5201800ec3d74711807c0
Eggisg <elisgustafsson06@gmail.com>
2025-11-21 16:24:09 +0100

    builds
    la till "Byggen" folder och byggde spelet

--- Assets/Scripts/killerWall.cs
+++ Assets/Scripts/killerWall.cs
@@ -4,6 +4,6 @@ public class killerWall : MonoBehaviour
 {
     void OnCollisionEnter2D(Collision2D other)
     {
-        SceneManager.LoadScene("Main");
+        SceneManager.LoadScene("Menu");
     }
 }
